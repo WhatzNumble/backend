4459e4736327673ffcacdb1ecc4d796cac47e133
bluewow <dogseen@gmail.com>
2022-04-18 22:32:51 +0900

    init

---
+++ src/main/java/com/numble/whatz/WhatzApplication.java
@@ -0,0 +1,13 @@
+package com.numble.whatz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WhatzApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WhatzApplication.class, args);
+    }
+
+}

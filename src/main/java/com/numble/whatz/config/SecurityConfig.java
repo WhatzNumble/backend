3de8eda242bbfc1d49838b48a3eb59b95b796be9
Lee-seungju <seungju0000@naver.com>
2022-04-19 20:56:51 +0900

    /login** 이외에는 카카오 인증을 하도록 바꿈.

--- src/main/java/com/numble/whatz/config/SecurityConfig.java
+++ src/main/java/com/numble/whatz/config/SecurityConfig.java
@@ -19,27 +19,22 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final CustomOAuth2UserService customOAuth2UserService;
 
-    @Bean
-    public BCryptPasswordEncoder bCryptPasswordEncoder() {
-        return new BCryptPasswordEncoder();
-    }
-
-    @Override
-    public void configure(WebSecurity web) throws Exception {
-//        web.ignoring().mvcMatchers("") // 이곳에 있는 파일들은 시큐리티 적용 무시
-    }
+//    @Override
+//    public void configure(WebSecurity web) throws Exception {
+//        web.ignoring().mvcMatchers("/login/**"); // 이곳에 있는 파일들은 시큐리티 적용 무시
+//    }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
+                .antMatchers("/login/**").permitAll()
                 .anyRequest()
-                .permitAll()
+                .authenticated()
             .and()
                 .logout()
                 .logoutSuccessUrl("/") // 로그아웃 이후
             .and()
                 .oauth2Login()
-                .defaultSuccessUrl("/login-success") // 로그인 성공 이후
                 .userInfoEndpoint()
                 .userService(customOAuth2UserService); // 로그인 성공하면 customOAuth2UserService에서 처리를 하겠다.
     }
--- src/main/java/com/numble/whatz/web/user/controller/UserController.java
+++ src/main/java/com/numble/whatz/web/user/controller/UserController.java
@@ -1,18 +1,41 @@
 package com.numble.whatz.web.user.controller;
 
+import com.numble.whatz.domain.User.service.SessionUser;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.servlet.http.HttpSession;
+
 @RestController
 @Slf4j
+@RequiredArgsConstructor
 public class UserController {
+
+    private final HttpSession session;
 
     @GetMapping("login-success")
     public String kakaoLogin() {
         System.out.println("success");
+        return "ok";
+    }
+
+    @GetMapping("/login/oauth2/kakao")
+    public String kakaoTest(Model model) {
+        System.out.println("s");
+
+        SessionUser user = (SessionUser) session.getAttribute("user");
+        if (user != null) {
+            log.info("user.getEmail={}", user.getEmail());
+            log.info("user.getName={}", user.getName());
+            log.info("user.getPicture={}", user.getPicture());
+        }
+
+
         return "ok";
     }
 }

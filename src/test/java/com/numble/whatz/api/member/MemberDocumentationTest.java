391a3d5e11fb1d1177046a30d1b16d1d73752b94
Lee-seungju <seungju0000@naver.com>
2022-05-06 10:47:36 +0900

    Member API 명세서

--- src/test/java/com/numble/whatz/api/member/MemberDocumentationTest.java
+++ src/test/java/com/numble/whatz/api/member/MemberDocumentationTest.java
@@ -1,12 +1,34 @@
 package com.numble.whatz.api.member;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.numble.whatz.application.member.controller.MemberDto;
+import com.numble.whatz.application.member.service.CrudMemberService;
+import com.numble.whatz.application.video.controller.dto.HomeDto;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.restdocs.AutoConfigureRestDocs;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
 import org.springframework.security.test.context.support.WithMockUser;
 import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.ResultActions;
+
+import java.security.Principal;
+
+import static com.numble.whatz.api.utils.ApiDocumentUtils.getDocumentRequest;
+import static com.numble.whatz.api.utils.ApiDocumentUtils.getDocumentResponse;
+import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.doReturn;
+import static org.springframework.restdocs.mockmvc.MockMvcRestDocumentation.document;
+import static org.springframework.restdocs.mockmvc.RestDocumentationRequestBuilders.get;
+import static org.springframework.restdocs.payload.PayloadDocumentation.fieldWithPath;
+import static org.springframework.restdocs.payload.PayloadDocumentation.responseFields;
+import static org.springframework.restdocs.request.RequestDocumentation.parameterWithName;
+import static org.springframework.restdocs.request.RequestDocumentation.requestParameters;
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.status;
 
 @AutoConfigureRestDocs
 @AutoConfigureMockMvc
@@ -20,5 +42,34 @@ public class MemberDocumentationTest {
     @Autowired
     private ObjectMapper objectMapper;
 
+    @MockBean
+    private CrudMemberService crudMemberService;
 
+    @Test
+    public void getMemberInfo() throws Exception {
+        MemberDto memberDto = new MemberDto("email1", "nickName1", "thumbnail1");
+
+        doReturn(memberDto).when(crudMemberService).getProfile(any());
+        Principal mockPrincipal = Mockito.mock(Principal.class);
+        Mockito.when(mockPrincipal.getName()).thenReturn("me");
+
+        //when
+        ResultActions result = this.mockMvc.perform(
+                get("/api/profile")
+                        .principal(mockPrincipal)
+                        .accept(MediaType.APPLICATION_JSON)
+        );
+
+        //then
+        result.andExpect(status().isOk())
+                .andDo(document("member-profile", // (4)
+                        getDocumentRequest(),
+                        getDocumentResponse(),
+                        responseFields(
+                                fieldWithPath("email").description("이메일"),
+                                fieldWithPath("nickName").description("닉네임"),
+                                fieldWithPath("thumbnailUrl").description("썸네일 경로")
+                        )
+                ));
+    }
 }

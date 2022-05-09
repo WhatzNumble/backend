73c35197fac8fb512aad502d326017a4b39d03ed
Lee-seungju <seungju0000@naver.com>
2022-05-09 19:25:36 +0900

    awskey 추가

--- src/main/java/com/numble/whatz/application/video/service/VideoService.java
+++ src/main/java/com/numble/whatz/application/video/service/VideoService.java
@@ -19,7 +19,6 @@ import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.io.IOException;
 import java.security.Principal;
 import java.util.ArrayList;
 import java.util.List;
